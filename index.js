4560e2c7c965caf598ce776a49beadf3ba7b6a9c
ChaiWeiCheng <weichengchai0705@gmail.com>
2023-11-09 16:46:16 +0800

    Created the base Node.js project

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+const PORT = process.env.PORT || 5050
+var startPage = "index.html";
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static("./public"));
+
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/public/" +startPage);
+
+})
+
+app.listen(PORT, function () {
+    console.log(`Demo project at: ${PORT}`);
+});
