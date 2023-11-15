f8e69891ff84413f98e6251bb6d4f712679f8cc0
ChaiWeiCheng <weichengchai0705@gmail.com>
2023-11-15 09:12:27 +0800

    Implemented the backend for login

--- index.js
+++ index.js
@@ -9,9 +9,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
 
-const { register } = require('./utils/UserUtil')
+const { register, login } = require('./utils/UserUtil')
 app.post('/register', register);
+// app.post('/login', login);
 
+// const { viewResources , addResource } = require('./utils/ResourceUtil')
+// app.get('/view-booking', viewResources);
+// app.post('/add-booking', addResource);
 
 
 app.get('/', (req, res) => {
