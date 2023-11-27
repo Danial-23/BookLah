f9988a48b7a51d94d0d48152b6a790c3225fdde1
ChaiWeiCheng <weichengchai0705@gmail.com>
2023-11-27 15:41:46 +0800

    new test case for login

--- tests/userTests.test.js
+++ tests/userTests.test.js
@@ -135,6 +135,24 @@ describe('Testing Login Function', () => {
         };
         await login(req, res);
     });
+    it('Should shows invalid credentials', async () => {
+        const req = {
+            body: {
+                email: "haha@gmail.com",
+                password: orgContent[0].password,
+            },
+        };
+        const res = {
+            status: function (code) {
+                expect(code).to.equal(500);
+            return this;
+            },
+            json: function (data) {
+                expect(data.message).to.equal('Invalid credentials!');
+            },
+        };
+        await login(req, res);
+    });
 });
 describe('Testing get all user', () => {
     const facilitiesFilePath = 'utils/users.json';
