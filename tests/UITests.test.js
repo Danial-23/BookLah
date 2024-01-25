3ddba04cbb971b0b65132e1cc8da5240666fd6de
ChaiWeiCheng <weichengchai0705@gmail.com>
2024-01-25 16:36:36 +0800

    selenium testing

--- tests/UITests.test.js
+++ tests/UITests.test.js
@@ -1,23 +1,23 @@
-const { app } = require('../index');
-const { Builder, By, Key, until } = require('selenium-webdriver');
-const { describe, it } = require('mocha');
-const { expect } = require('chai');
-const chrome = require('selenium-webdriver/chrome');
-const chromeOptions = new chrome.Options();
-chromeOptions.addArguments('--headless');
-const driver = new Builder().forBrowser('chrome').setChromeOptions(chromeOptions).build();
-var server;
-before(async function () {
-    server = await new Promise((resolve) => {
-        server = app.listen(0, 'localhost', () => {
-            resolve(server);
-        });
-    })
-});
-describe('Testing Login UI', function () {
-});
-after(async function () {
-    await driver.quit();
-    await server.close();
-    process.exit(0);
-});
+// const { app } = require('../index');
+// const { Builder, By, Key, until } = require('selenium-webdriver');
+// const { describe, it } = require('mocha');
+// const { expect } = require('chai');
+// const chrome = require('selenium-webdriver/chrome');
+// const chromeOptions = new chrome.Options();
+// chromeOptions.addArguments('--headless');
+// const driver = new Builder().forBrowser('chrome').setChromeOptions(chromeOptions).build();
+// var server;
+// before(async function () {
+//     server = await new Promise((resolve) => {
+//         server = app.listen(0, 'localhost', () => {
+//             resolve(server);
+//         });
+//     })
+// });
+// describe('Testing Login UI', function () {
+// });
+// after(async function () {
+//     await driver.quit();
+//     await server.close();
+//     process.exit(0);
+// });
