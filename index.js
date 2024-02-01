3d39db8a25db92dc712d09267a59075de9ea5466
Danial-23 <ashfiq753@gmail.com>
2024-02-01 17:15:34 +0800

    Added Logging and Monitoring functionalities

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+const logger = require('./logger');
 
 const PORT = process.env.PORT || 5050
 var startPage = "index.html";
@@ -8,6 +9,9 @@ var startPage = "index.html";
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
+
+const statusMonitor = require('express-status-monitor');
+app.use(statusMonitor());
 
 const { register, login,getAllUsers } = require('./utils/userUtil')
 app.post('/register', register);
@@ -34,8 +38,10 @@ app.get('/', (req, res) => {
 
 })
 
-const server=app.listen(PORT, function () {
+const server = app.listen(PORT, function () {
     console.log(`Demo project at: ${PORT}`);
+    logger.info(`Demo project at: ${PORT}!`);
+    logger.error(`Example or error log`)
 });
 
 module.exports={app,server}
